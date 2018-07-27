a09bbe23a57b2b012c807a1ad209a13aa72e50de
aubykhan <aubykhan@gmail.com>
2018-07-27 08:28:55 +0500

    Add cards, listTile, stepper and text fields samples

--- lib/main.dart
+++ lib/main.dart
@@ -6,11 +6,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'Flutter Demo',
+      title: 'I/O Extended \'18',
       theme: new ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: new MyHomePage(title: 'Flutter Demo Home Page'),
+      home: new MyHomePage(title: 'I/O Extended \'18'),
     );
   }
 }
@@ -35,24 +35,113 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    var listTile = ListTile(
+      leading: CircleAvatar(child: Icon(Icons.person)),
+      title: Text('Asadullah Yousuf'),
+      subtitle: Text('Need for speed? Keep calm and use Flutter'),
+      trailing: Icon(Icons.play_circle_outline, color: Colors.green),
+    );
+
+    var cards = ListView(
+      children: <Widget>[
+        Card(
+          color: Colors.green,
+          child: Container(
+            height: 150.0,
+            child: Center(child: Text('Green')),
+          ),
+        ),
+        Card(
+          child: Container(
+            height: 150.0,
+            child: Center(
+              child: Text('White'),
+            ),
+          ),
+        ),
+        Card(
+          color: Colors.blue,
+          child: Container(
+            height: 150.0,
+            child: Center(
+              child: Text('Blue'),
+            ),
+          ),
+        ),
+      ],
+    );
+
+    var textFields = Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        ListTile(
+          leading: CircleAvatar(child: Icon(Icons.person)),
+        ),
+        TextField(
+          decoration: InputDecoration(
+              labelText: 'Underline border', hintText: 'Type something'),
+        ),
+        SizedBox(height: 16.0),
+        TextField(
+          decoration: InputDecoration(
+              filled: true,
+              labelText: 'Underline border',
+              hintText: 'Type something'),
+        ),
+        SizedBox(height: 16.0),
+        TextField(
+          decoration: InputDecoration(
+              filled: true,
+              labelText: 'Bordered',
+              border: OutlineInputBorder(),
+              hintText: 'Type something'),
+        ),
+        SizedBox(height: 16.0),
+        TextField(
+          decoration: InputDecoration(
+              filled: true,
+              labelText: 'Rounded',
+              border:
+                  OutlineInputBorder(borderRadius: BorderRadius.circular(30.0)),
+              hintText: 'Type something'),
+        ),
+      ],
+    );
+
+    var stepper = Stepper(
+        currentStep: 1,
+        onStepContinue: () {},
+        onStepCancel: () {},
+        steps: <Step>[
+          Step(
+              isActive: true,
+              state: StepState.complete,
+              title: const Text('Step 1'),
+              subtitle: const Text('This is step 1'),
+              content: Center(
+                child: Text('Step 1 contents'),
+              )),
+          Step(
+              isActive: true,
+              state: StepState.editing,
+              title: const Text('Step 2'),
+              subtitle: const Text('This is step 2'),
+              content: Center(
+                child: Text('Step 2 contents'),
+              )),
+          Step(
+              title: const Text('Step 3'),
+              subtitle: const Text('This is step 3'),
+              content: Center(
+                child: Text('Step 3 contents'),
+              )),
+        ]);
+
     return new Scaffold(
       appBar: new AppBar(
         title: new Text(widget.title),
       ),
-      body: new Center(
-        child: new Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new Text(
-              'You have pushed the button this many times:',
-            ),
-            new Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
+      body: stepper,
       floatingActionButton: new FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
